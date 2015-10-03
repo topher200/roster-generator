d62f7c468b42365d9db77c9348acd2a41a28045d
Topher Brown <chris.brown@farsounder.com>
2015-10-03 02:10:22 -0400

    move error checking and file reading into utils

---
+++ file_util.go
@@ -0,0 +1,37 @@
+// File operations. Retrieve players from csv
+
+package main
+
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+	"unicode/utf8"
+
+	"github.com/topher200/baseutil"
+)
+
+func ParsePlayers(inputFilename string) []Player {
+	// Open our input Players file
+	file, err := os.Open(inputFilename)
+	baseutil.Check(err)
+	defer file.Close()
+
+	// Read in our csv. Throw away the header. We expect the input to be of the
+	// form (player name, value, gender ('m' or 'f'))
+	playersCsv := csv.NewReader(file)
+	_, err = playersCsv.Read()
+	baseutil.Check(err)
+
+	// Read in all players
+	playersCsvLines, err := playersCsv.ReadAll()
+	players := make([]Player, len(playersCsvLines))
+	baseutil.Check(err)
+	for i, player := range playersCsvLines {
+		value, err := strconv.ParseFloat(player[1], 32)
+		baseutil.Check(err)
+		gender, _ := utf8.DecodeRuneInString(player[2])
+		players[i] = Player{player[0], float32(value), gender, 0}
+	}
+	return players
+}
--- roster_generator.go
+++ roster_generator.go
@@ -3,19 +3,9 @@
 package main
 
 import (
-	"encoding/csv"
 	"flag"
 	"fmt"
-	"os"
-	"strconv"
-	"unicode/utf8"
 )
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 
 type Player struct {
 	name   string
@@ -26,31 +16,6 @@ type Player struct {
 
 type Solution struct {
 	players []Player
-}
-
-func ParsePlayers(inputFilename string) []Player {
-	// Open our input Players file
-	file, err := os.Open(inputFilename)
-	check(err)
-	defer file.Close()
-
-	// Read in our csv. Throw away the header. We expect the input to be of the
-	// form (player name, value, gender ('m' or 'f'))
-	playersCsv := csv.NewReader(file)
-	_, err = playersCsv.Read()
-	check(err)
-
-	// Read in all players
-	playersCsvLines, err := playersCsv.ReadAll()
-	players := make([]Player, len(playersCsvLines))
-	check(err)
-	for i, player := range playersCsvLines {
-		value, err := strconv.ParseFloat(player[1], 32)
-		check(err)
-		gender, _ := utf8.DecodeRuneInString(player[2])
-		players[i] = Player{player[0], float32(value), gender, 0}
-	}
-	return players
 }
 
 func main() {
