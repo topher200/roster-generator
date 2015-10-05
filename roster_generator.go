9d6758a628232e9cb89c709e731a898e030ff950
Topher Brown <chris.brown@farsounder.com>
2015-10-05 08:35:29 -0400

    implement playerDifference calculation

--- roster_generator.go
+++ roster_generator.go
@@ -5,6 +5,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 )
 
 const numTeams = 6
@@ -28,8 +29,22 @@ func splitIntoTeams(players []Player) [][]Player {
 // Score a solution based on weighted critera.
 func score(solution Solution) int {
 	// Balanced by number
+	teams := splitIntoTeams(solution.players)
+
+	// We sum the amount of difference (in number of players on the team) between
+	// every team and every other team.
+	//
+	// This is currently incredibly inefficient (O(n^2) (but n is 6!))
+	playerDifference := 0
+	for _, team := range teams {
+		for _, otherTeam := range teams {
+			playerDifference += int(math.Abs(float64(len(team) - len(otherTeam))))
+		}
+	}
+
 	// Balanced by gender
-	return 0
+
+	return playerDifference
 }
 
 func main() {
@@ -38,7 +53,8 @@ func main() {
 		"input-file", "input-test.txt", "filename to read input")
 	flag.Parse()
 
-	playerList := ParsePlayers(*filenamePointer)
+	players := ParsePlayers(*filenamePointer)
+	solution := Solution{players}
 
-	fmt.Println(playerList)
+	fmt.Println(score(solution))
 }
