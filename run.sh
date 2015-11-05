9b3ac5931355535f857c638d95a04ead85b5f108
Topher Brown <topher200@gmail.com>
2015-11-05 02:04:08 -0500

    improve readibility of run script

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-go test && go build && ./roster-generator sample_players.txt sample_baggages.txt
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Test, build, then run with our sample data
+go test
+go build
+./roster-generator sample_players.txt sample_baggages.txt
