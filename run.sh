674d63a03757ede6dceb86e3072bf39fe37cf71c
Topher Brown <topher200@gmail.com>
2015-11-06 01:41:09 -0500

    enable debug mode

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ set -e
 
 # Test, build, then run with our sample data
 go test
-go build
-./roster-generator sample_players.txt sample_baggages.txt
+godebug build
+./roster-generator.debug sample_players.txt sample_baggages.txt
