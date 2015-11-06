d60a3cc1e7396d7cdac9c20625550a4099ffa988
Topher Brown <topher200@gmail.com>
2015-11-06 02:23:42 -0500

    Revert "enable debug mode"
    This reverts commit 8edc348f8ce15e0a101f2ea94fde82b6fd81c409.

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ set -e
 
 # Test, build, then run with our sample data
 go test
-godebug build
-./roster-generator.debug sample_players.txt sample_baggages.txt
+go build
+./roster-generator sample_players.txt sample_baggages.txt
